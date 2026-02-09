44ebb4a4831f2b5fa2fa11b187018a452d1e5061
Gokul M R <gokulmr331@gmail.com>
2026-02-09 21:18:05 +0530

    feat: user registering done

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -1,18 +1,41 @@
 import { Request, Response } from "express";
+import userModel from "../models/userModels";
+import bcrypt from "bcryptjs";
 
 
 export const signup = async (req: Request, res: Response) => {
     try {
-        // Implement your signup logic here 
-        res.status(201).json({ message: "User registered successfully" });
+        const { username, email, mobile, password } = req.body;
+
+        if (!username || !email || !password || !mobile) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        const alreadyExists = await userModel.findOne({ email, username, mobile })
+        if (alreadyExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await userModel.create({
+            data: {
+                name: username,
+                email,
+                phone: mobile,
+                password: hashedPassword
+            }
+        });
+        return res.status(201).json({ message: "User registered successfully", newUser });
     } catch (error) {
         res.status(500).json({ message: "Internal server error" });
     }
 }
 
+
 export const login = async (req: Request, res: Response) => {
     try {
-        // Implement your login logic here  
+          
         res.status(200).json({ message: "User logged in successfully" });
     } catch (error) {
         res.status(500).json({ message: "Internal server error" });
---
+++ backend/src/models/userModels.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    phone: string;
+    password: string;
+    createdAt: Date;
+    role: string;
+    status?: string;
+    updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        name: { type: String, required: true, trim: true },
+        email: { type: String, required: true, unique: true, lowercase: true},
+        phone: { type: String, required: true },
+        password: { type: String, required: true },
+        role: { type: String, enum: ['user', 'admin'], default: 'user' },
+        status: { type: String, enum: ['active', 'inactive'], default: 'active' },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+    },
+    { timestamps: true }
+);
+
+const userModel = mongoose.model<IUser>("User", userSchema);
+
+export default userModel;
