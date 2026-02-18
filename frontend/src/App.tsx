2f13e2d511f38df3ddfb50c3a90ec786b4913312
Gokul M R <gokulmr331@gmail.com>
2026-02-18 21:50:31 +0530

    update: react page component basic setup done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 
 const App = () => {
   return (
-    <div className='bg-amber-400 h-200 w-100 shadow-2xl'>
+    <div className="bg-amber-100 h-screen w-screen flex items-center justify-center">
       
     </div>
   )
